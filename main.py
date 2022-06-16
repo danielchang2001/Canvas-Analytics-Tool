3f853526802991f3bddd0da210b038fd13eb3726
Daniel Chang <danielchang083@gmail.com>
2022-06-16 00:21:52 -0700

    Cleaned up UI

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ from bs4 import BeautifulSoup
 
 def main():
     # Login input
-    email = raw_input("Enter your CruzID: ")
+    email = raw_input("\nEnter your CruzID: ")
     password = getpass("Enter your Gold password: ")
-    twoAuth = raw_input("Choose preferred 2FA (push, phone, pass): ")
+    customWeights = raw_input("\nWill you be entering custom weights? (y/n): ")
+    if (customWeights == 'y') or (customWeights == 'yes') or (customWeights == 'Y') or (customWeights == 'Yes'):
+        print("\n     You will be prompted to enter custom weights once the page loads.")
+    twoAuth = raw_input("\nChoose preferred 2FA (push, phone, pass): ")
+    if twoAuth == 'phone':
+        print("\n     Press 1 when your phone rings.")
+    if twoAuth == 'push':
+        print("\n     Push accept on the Duo app.")
     if twoAuth == 'pass':
-        print("Since pass requires you to enter the password manually, you will have to complete the two factor authentication yourself.")
-    customWeights = raw_input("In case the weights on Canvas are inaccurate, would you like to enter in custom weights? (y/n): ")
+        print("\n     Since pass requires you to enter the password manually, you will have to complete the two factor authentication yourself.")
     # Setup webdriver
     driver = webdriver.Firefox(executable_path='./geckodriver')
     driver.implicitly_wait(8)
@@ -71,15 +77,16 @@ def main():
     for context, percent in zip(weightRow, weightRow2):
         if str(context.get_attribute('innerHTML')) == 'Total':
             continue
-        customWeight = raw_input("Enter custom weight for '" + str(context.get_attribute('innerHTML')) + "' category (eg. type 40 if 40%): ")
+        if (customWeights == 'y') or (customWeights == 'yes') or (customWeights == 'Y') or (customWeights == 'Yes'):
+            customWeight = raw_input("Enter custom weight for '" + str(context.get_attribute('innerHTML')) + "' category (eg. type 40 if 40%): ")
+            customWeightDict[str(context.get_attribute('innerHTML'))] = float(customWeight)  / 100
         weightDict[str(context.get_attribute('innerHTML'))] = float(((percent.get_attribute('innerHTML')).split('%'))[0]) / 100
-        customWeightDict[str(context.get_attribute('innerHTML'))] = float(customWeight)  / 100
         pointsDict[str(context.get_attribute('innerHTML'))] = 0
         avgDict[str(context.get_attribute('innerHTML'))] = 0
         realAvgDict[str(context.get_attribute('innerHTML'))] = 0
         userDict[str(context.get_attribute('innerHTML'))] = 0
         realUserDict[str(context.get_attribute('innerHTML'))] = 0
-    print(weightDict)
+    # print(weightDict)
 
     # Find each score details table
     tables = driver.find_elements(By.CLASS_NAME, 'score_details_table')
@@ -93,7 +100,7 @@ def main():
         # Skip if table is comment not score table
         if '<span' in avg:
             continue
-        print("avg: ", float(avg))
+        # print("avg: ", float(avg))
         driver.implicitly_wait(3)
 
         # Find user grade
@@ -115,19 +122,19 @@ def main():
             title = titleEl.get_attribute("innerHTML")
             contextEl = tr.find_element(By.CLASS_NAME, 'context')
             context = contextEl.get_attribute("innerHTML")
-            print(title)
-            print(context)
+            # print(title)
+            # print(context)
             
             # Find total possible points for each score
             pointsPossible = tr.find_element(By.CLASS_NAME, 'points_possible')
             pointsPos = pointsPossible.text
-            print("out of: ", float(pointsPos))
+            # print("out of: ", float(pointsPos))
 
             # Find user's score
             gradeParent = tr.find_element(By.CLASS_NAME, 'grade')
             userGrade = gradeParent.get_attribute('innerHTML').split('</span>\n                  ')
             userGradeF = userGrade[-1].split('\n')[0]
-            print("grade:", float(userGradeF))
+            # print("grade:", float(userGradeF))
 
             # Adds up total average points and total points possible and puts them in dictionaries
             for k in weightDict.keys():
@@ -138,30 +145,27 @@ def main():
 
         else: # Score ID empty
             continue
-        
-        print("-----------------\n")
     
-    print(weightDict)
+    # print(weightDict)
     # Calculate the average class score using total average scores and total points possible for each grade category. 
     # The total avg percentage for each category is then multiplied by the respective weights.
     avgGradeF = 0
     userGradeF = 0
     for k in weightDict.keys():
-        if customWeights == ('y' or 'yes' or 'Y' or 'Yes'):
+        if (customWeights == 'y') or (customWeights == 'yes') or (customWeights == 'Y') or (customWeights == 'Yes'):
             realAvgDict[k] = (float(avgDict[k]) / float(pointsDict[k])) * 100 * customWeightDict[k]
             realUserDict[k] = (float(userDict[k]) / float(pointsDict[k])) * 100 * customWeightDict[k]
-        if customWeights == ('n' or 'no' or 'N' or 'No'):
+        if customWeights == 'n' or customWeights == 'no' or customWeights == 'N' or customWeights == 'No':
             realAvgDict[k] = (float(avgDict[k]) / float(pointsDict[k])) * 100 * weightDict[k]
             realUserDict[k] = (float(userDict[k]) / float(pointsDict[k])) * 100 * weightDict[k]
         avgGradeF += realAvgDict[k]
         userGradeF += realUserDict[k]
+    print("\n\n==============================================")
+    print("===== Average class grade Vs. Your grade =====")
+    print("==============================================")
+    print("\nCurrent average class grade (without counting any extra credit) is " + str(avgGradeF))
+    print("\nYour current grade (without counting any extra credit) is " + str(userGradeF))
+    print("\n")
 
-    avgGradeF = avgGradeF / 100
-    userGradeF = userGradeF / 100
-    print(pointsDict)
-    print(avgDict)
-    print(realAvgDict)
-    print(avgGradeF)
-    print(userGradeF)
 main()
 
